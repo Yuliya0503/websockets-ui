1435891a7763802b89edd79114d067eb3f84000d
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-07-06 18:24:52 +0200

    fix: change settings for task

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  parserOptions: {
+    tsconfigRootDir : __dirname,
+    sourceType: 'module'
+  },
+  root: true,
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+}
