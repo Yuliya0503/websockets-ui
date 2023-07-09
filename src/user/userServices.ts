412f7dc27ab2e1b3622137517443a5b045377eb7
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-07-09 18:41:03 +0200

    feat: add hundler winnrs to user services

--- src/user/userServices.ts
+++ src/user/userServices.ts
@@ -1,10 +1,12 @@
 import { WebSocket } from 'ws';
 import User from './user';
-import { IAuthenticatedWS } from '../models/iuser';
+import { IAuthenticatedWS, IWunner } from '../models/iuser';
 import { IReg } from '../models/out';
+
 
 export default class UserServices {
   private users: User[] = [];
+  private winners: IWunner[] = [];
 
   public register(name: string, pass: string, ws: WebSocket): IReg {
     const isUserExist = this.users.find(({ name: isNameExist }) => {
@@ -33,6 +35,7 @@ export default class UserServices {
       (ws as IAuthenticatedWS).name = user.name;
       (ws as IAuthenticatedWS).index = user.index;
       this.users.push(user);
+      this.winners.push({ name, wins: 0 });
 
       return {
         name: user.name,
@@ -41,5 +44,21 @@ export default class UserServices {
         errorText: '',
       };
     }
+  }
+
+  public getWinners(): IWunner[] {
+    return this.winners;
+  }
+
+  public winnerProc(playerId: number): void {
+    const user = this.users.find(({index}) => {
+      return (index === playerId)
+    }) as User;
+    
+    this.winners = this.winners.map(({ name, wins}) => {
+      return ((name === user.name) ? {name, wins: wins +1} : {name, wins})
+    }).sort((winI, winJ) => {
+      return (winJ.wins - winI.wins)
+    });
   }
 }
