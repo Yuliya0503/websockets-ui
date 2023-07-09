0ea1c314d1e812f5f53cbb2d75c8f193ce2ad641
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-07-09 16:47:17 +0200

    feat: add function of parsing the raw incoming message

---
+++ src/helpers/parseRawIncMess.ts
@@ -0,0 +1,15 @@
+import { IIncomingMessage } from "src/models/inCommands";
+
+export const parseRawIncommingMessage = (message: string) => {
+  try {
+    const { data: rawData, ...restMessage } = JSON.parse(message);
+    const data = rawData === '' ? rawData : JSON.parse(rawData);
+    return {
+      ...restMessage,
+      data,
+    } as IIncomingMessage;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
