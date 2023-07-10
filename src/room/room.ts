7ff80767e13664b96bd188abce6bc5269da8741e
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-07-10 15:08:59 +0200

    feat: add game class to room

--- src/room/room.ts
+++ src/room/room.ts
@@ -1,10 +1,11 @@
 import { IRoom, IUser, IAuthenticatedWS } from '../models/iuser';
-
+import Game from '../geme_control/game';
 export default class Room implements IRoom {
   private static index = 0;
   public roomId: number;
   public roomUsers: IUser[] = [];
   public sockets: IAuthenticatedWS[] = [];
+  public game: Game;
 
   constructor(ws: IAuthenticatedWS) {
     this.roomId = Room.index;
